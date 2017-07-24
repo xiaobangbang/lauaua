a87dc956d57cf33af8ee211ad74b075c07448927
xiaobangbang <1196855008@qq.com>
2017-07-25 00:34:48 +0800

    Add files via upload

---
+++ choose_acct.php
@@ -0,0 +1,106 @@
+<html>
+ <head>
+ <script type="text/javascript"> 
+function check_all(obj,cName) 
+{ 
+    var checkboxs = document.getElementsByName(cName); 
+    for(var i=0;i<checkboxs.length;i++){checkboxs[i].checked = obj.checked;} 
+} 
+function check_submit(cName) 
+{ 
+	//alert(cName);
+	var valid = 0;
+    var checkboxs = document.getElementsByName(cName); 
+	 for(var i=0;i<checkboxs.length;i++){		
+		if (checkboxs[i].checked ==true) {
+			//myform.submit();
+			valid = 1;			
+		}
+	}
+	
+	if (valid==1){		
+		//document.getElementsByName("myform").submit();
+		//alert(document.getElementsByName("myform"));	
+		//document.getElementsById("myform").submit()
+	}else{
+		alert("没有选择账号");
+	}
+	
+} 
+</script> 
+ </head>
+ <body>
+  <form id="myform"  enctype="multipart/form-data" action="download-csv.php" method="post">
+  <p><input type="checkbox" name="all" onclick="check_all(this,'hobby[]')" />全选/全不选</p> 
+<?php
+/**
+ * Simple example of extending the SQLite3 class and changing the __construct
+ * parameters, then using the open method to initialize the DB.
+ */
+ 
+
+ 
+class MyDB extends SQLite3
+{
+    function __construct()
+    {
+        $this->open('D:\XXT_Lan_2.5.5.0\data\foo2.db');
+    }
+	
+	function generate_password( $length = 8 ) { 
+// 密码字符集，可任意添加你需要的字符 
+$chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_ []{}<>~`+=,.;:/?|'; 
+$password =''; 
+for ( $i = 0; $i < $length; $i++ ) 
+{ 
+// 这里提供两种字符获取方式 
+// 第一种是使用 substr 截取$chars中的任意一位字符； 
+// 第二种是取字符数组 $chars 的任意元素 
+// $password .= substr($chars, mt_rand(0, strlen($chars) – 1), 1); 
+$password .= $chars[ mt_rand(0, strlen($chars) - 1) ]; 
+} 
+return $password; 
+} 
+
+}
+
+$db = new MyDB();
+
+
+
+/**
+$db->exec('CREATE TABLE foo (bar STRING)');
+*/
+$v_acct = $db->generate_password(9);
+//echo $v_acct;
+//$db->exec("INSERT INTO foo (acct_user,user_pwd,bar) VALUES ('$v_acct','pwd1','This is a test')");
+
+$result = $db->query('SELECT * FROM excel_acct_info');
+/**
+var_dump();
+*/
+echo "<TABLE border='1' bgcolor='yellow'>";
+while($row= $result->fetchArray()){
+	$acct_user= $row['acct_user'];
+	$user_pwd= $row['user_pwd'];
+	$v_csv_data=$acct_user.','.$user_pwd;
+	echo "<tr>";
+	echo "<td>";	
+	//echo '<input type="checkbox" name="hobby[]" value='.$row['acct_user'].'/>'.$row['acct_user'].'/'.$row['user_pwd'];
+	echo "<input type='checkbox' name='hobby[]' value='$v_csv_data'/>";
+	echo "</td>";
+	echo "<td>";		
+	echo "$acct_user";
+	echo "</td>";
+	echo "<td>";
+	echo "$user_pwd";
+	echo "</td>";
+	echo "</tr>";
+}	
+echo "</TABLE>";
+?> 
+ <input type="submit" value="提交" onclick="check_submit('hobby[]')"/>
+ <input type="button" value="刷新当前页面" onclick="javascript:location.reload();" />
+ </form>  
+ </body>
+</html>
---
+++ download-csv.php
@@ -0,0 +1,98 @@
+<?php
+$csv_data_path = 'D:\\XXT_Lan_2.5.5.0\\data\\';
+/**
+ * PHPExcel
+ *
+ * Copyright (c) 2006 - 2015 PHPExcel
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @category   PHPExcel
+ * @package    PHPExcel
+ * @copyright  Copyright (c) 2006 - 2015 PHPExcel (http://www.codeplex.com/PHPExcel)
+ * @license    http://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt	LGPL
+ * @version    ##VERSION##, ##DATE##
+ */
+
+/** Error reporting */
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
+//date_default_timezone_set('Europe/London');
+
+if (PHP_SAPI == 'cli')
+	die('This example should only be run from a Web Browser');
+
+/** Include PHPExcel */
+require_once dirname(__FILE__) . '/Classes/PHPExcel.php';
+
+
+// Create new PHPExcel object
+$objPHPExcel = new PHPExcel();
+
+// Set document properties
+$objPHPExcel->getProperties()->setCreator("Maarten Balliauw")
+							 ->setLastModifiedBy("Maarten Balliauw")
+							 ->setTitle("Office 2007 XLSX Test Document")
+							 ->setSubject("Office 2007 XLSX Test Document")
+							 ->setDescription("Test document for Office 2007 XLSX, generated using PHP classes.")
+							 ->setKeywords("office 2007 openxml php")
+							 ->setCategory("Test result file");
+
+
+$ROW_NUM =1;
+$strs = array();
+foreach( $_POST['hobby'] as $i)
+{
+	$strs = explode(',',$i);
+	$objPHPExcel->setActiveSheetIndex(0)->setCellValue('A'.$ROW_NUM, $strs[0]);
+	$objPHPExcel->setActiveSheetIndex(0)->setCellValue('B'.$ROW_NUM, $strs[1]);
+	$ROW_NUM= $ROW_NUM +1;
+	//echo '<br>';
+ //$result .= $i;
+}
+
+$objPHPExcel->getActiveSheet()->setTitle('Simple22');
+
+
+// Set active sheet index to the first sheet, so Excel opens this as the first sheet
+$objPHPExcel->setActiveSheetIndex(0);
+
+//ob_end_clean();
+// Redirect output to a client’s web browser (Excel5)
+header('Content-Type: application/vnd.ms-excel;charset=UTF-8');
+header('Content-Disposition: attachment;filename="待运行账号.xlsx"');
+header('Cache-Control: max-age=0');
+// If you're serving to IE 9, then the following may be needed
+header('Cache-Control: max-age=1');
+
+// If you're serving to IE over SSL, then the following may be needed
+header ('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+header ('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT'); // always modified
+header ('Cache-Control: cache, must-revalidate'); // HTTP/1.1
+header ('Pragma: public'); // HTTP/1.0
+
+//$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
+$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'CSV');
+//$objWriter->save('php://output');
+$str = date('Y-m-d_His');
+    $file_name = $str . ".csv";
+$objWriter->save($csv_data_path.$file_name);
+
+$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+$objWriter->save('php://output');
+
+exit;
+?>
